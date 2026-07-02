9b8378e7537794cf9926d6cc3f93c75779703079
yamissobrng <yamisobrng@icloud.com>
2026-07-02 16:14:05 +0300

    Added new files

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(
+        request,
+        'articles/index.html',
+        context={'name': 'article'},
+    )
