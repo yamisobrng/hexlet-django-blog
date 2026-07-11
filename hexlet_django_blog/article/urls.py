daeaddff154fbdf30c8967dba0be774e49ad6082
yamissobrng <yamisobrng@icloud.com>
2026-07-11 21:03:30 +0300

    Added new files

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView
 urlpatterns = [
     path('', IndexView.as_view()),
-    path('<int:id>/', ArticleView.as_view()),
+    path('<int:id>/', ArticleView.as_view(), name='article'),
     path('<int:article_id>/comments/<int:id>', ArticleCommentsView.as_view()),
 ]
